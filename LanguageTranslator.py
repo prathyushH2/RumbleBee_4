a00511a52fd184eb071e3ca8c7748cf7a33f858f
prathyushH2 <122345198+prathyushH2@users.noreply.github.com>
2023-06-17 10:57:47 +0530

    Update LanguageTranslator.py

--- LanguageTranslator.py
+++ LanguageTranslator.py
@@ -10,7 +10,7 @@ def translate_text(text, target_language):
     return translated_text.text
 
 text_to_translate = "Hello, how are you?"
-target_language = choose_lang()  # French
+target_language = choose_lang()  # E.g., French
 
 translated_text = translate_text(text_to_translate, target_language)
 print(f"Translated text: {translated_text}")
