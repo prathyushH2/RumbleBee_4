5737268188f57acce5aae283c7b22f2a8c125a75
prathyushH2 <122345198+prathyushH2@users.noreply.github.com>
2023-06-17 10:52:44 +0530

    Add files via upload

---
+++ EmailContentWriter.py
@@ -0,0 +1,49 @@
+import openai
+
+def generate_email_response(email_content, reply_content):
+    openai.api_key = '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'  # Replace with your OpenAI API key
+
+    prompt = f"You received an email:\n\n{email_content}\n\nReply: {reply_content}\n\n"
+    response = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=prompt,
+        max_tokens=150,
+        temperature=0.7,
+        n=1,
+        stop=None,
+    )
+
+    generated_text = response.choices[0].text.strip()
+    
+    # Check if response is incomplete and generate additional tokens if necessary
+    while not (generated_text.endswith(".") or generated_text.endswith("?") or generated_text.endswith("\n")):
+        response = openai.Completion.create(
+            engine='text-davinci-003',
+            prompt=prompt + generated_text,
+            max_tokens=50,
+            temperature=0.7,
+            n=1,
+            stop=None,
+        )
+        generated_text += response.choices[0].text.strip()
+
+    return generated_text
+
+# Example usage
+email_content = """
+    Dear Utkarsh,
+
+Good Morning!
+
+We are thrilled to have you as a participant in the upcoming hackathon event, and we appreciate your enthusiasm for exploring innovative solutions. As part of the lab exercises and to ensure a seamless experience during the event, we kindly request your assistance in signing in to your AWS account using the provided CSV file.
+
+In case you encounter any difficulties during the account sign-in process or have questions related to the AWS console, please don't hesitate to reach out to our organisers team. We are here to provide guidance and address any concerns you may have.
+
+Thank you for your cooperation and active participation. Should you require any further information or assistance, please feel free to contact us. We wish you the best of luck in the hackathon and hope you have a rewarding and fulfilling experience.
+
+Regards,
+Anantha 
+"""
+reply_content = "Thank you for your help with aws access."
+generated_response = generate_email_response(email_content, reply_content)
+print(generated_response)
---
+++ LanguageTranslator.py
@@ -0,0 +1,18 @@
+from googletrans import Translator
+
+def choose_lang():
+    target_lang=input('Enter the target language (e.g.,en, fr, de):')
+    return target_lang
+
+def translate_text(text, target_language):
+    translator = Translator()
+    translated_text = translator.translate(text, dest=target_language)
+    return translated_text.text
+
+text_to_translate = "Hello, how are you?"
+target_language = choose_lang()  # French
+
+translated_text = translate_text(text_to_translate, target_language)
+print(f"Translated text: {translated_text}")
+
+# pip install googletrans==4.0.0-rc1
